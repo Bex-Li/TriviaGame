24fcd8d9d155766c3c9d637fa204034a3aa67209
Bex-Li <rebekkikko@gmail.com>
2018-04-03 23:24:20 -0700

    updated js and html files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,112 @@
-You'll create a trivia form with multiple choice or true/false options 
-(your choice).
+var sixtySecNum = 25;
+var intervalId;
+var correctAnsCounter = 0;
+var incorrAnsCounter = 0;
 
-The player will have a limited amount of time to finish the quiz.
+var multiQuestion = [
+  "The shallow fog that often engulfs San Francisco and the iconic Golden Gate Bridge goes by what name by locals?",
+  "Which San Francisco neighborhood is home to Coit Tower and the best pizza and pasta in town?",
+  "The Painted-______ are one of the best-known row of Victorian houses across from Alamo Square Park in Hayes Valley.",
+  "Which island can you find in the Bay of the Bay Area?"
+];
 
-The game ends when the time runs out. The page will reveal the number 
-of questions that players answer correctly and incorrectly.
+var unAnsweredCounter = multiQuestion.length;
 
-Don't let the player pick more than one answer per question.
+var multiAnswers = [
+  ["Karl",
+  "Frank",
+  "Steve",
+  "Joe"],
+  ["Northbeach",
+  "SoMa",
+  "Mission",
+  "Nob Hill"],
+  ["Ladies",
+  "Homes",
+  "Families",
+  "Perfect"],
+  ["Angel",
+  "Devil's",
+  "Easter",
+  "Carribean"]
+];
+var correctAnswers = [
+  "Karl",
+  "Northbeach",
+  "Ladies",
+  "Angel",
+];
 
-Don't forget to include a countdown timer.
+$("#start").on("click", start);
+$("#stop").on("click", stop);
+$(".question").on("click", "input", checkAnswers);
 
-Pseudo Code: 
+function start() {
+  $("#start").hide();
+  run();
+  writeQuestion();
+  $("#stop").append($('<input id="stop" type="button" class="w3-btn w3-round-xlarge" value="STOP">'));
+}
 
-Start Button - on click 
-    // Timer starts
-    //     counting down timer from 1 min 
-        (after start click create hide function)
+function run() {
+  clearInterval(setInterval)
+  intervalId = setInterval(decrement, 1000);
+}
 
-Triva Questions are asked
-    List of multiple questions (hardcode in HTML, or with an array in JS & jQeury)
-    List of multiple choice answers (with an array in JS & jQeury)
+function decrement() {
+  sixtySecNum--;
+  $("#timerDiv").html("<h2>" + sixtySecNum + "</h2>");
+  if (sixtySecNum === 0) {
+    stop();
+  }
+}
 
-User answers Trivia Questions
-    with a condition of if and else 
-        What to do if the user answers all questiosn in <60 seconds
-            Stop Button 
-                on click listener 
-                after stop click create hide function on click listener
-            Got to results page 
-                Display Answered Questions Counter
-                    Correct Answered
-                    Incorrectly Answered
-                    Unanswered
-        What to do if the user answers all questiosn in >60 seconds
-            Got to results page 
-                Display Answered Questions Counter
-                    Correct Answered 
-                    Incorrectly Answered
-                    Unanswered
+function stop() {
+  clearInterval(intervalId);
+  $(".question").hide();
+  $("#timerDiv").hide();
+  $(".counters").html("Correct Answers: " + correctAnsCounter + "<br>");
+  $(".counters").append("Wrong Answers: " + incorrAnsCounter + "<br>"); 
+  $(".counters").append("You Didn't Answer: " + unAnsweredCounter + " questions" + "<br>"); 
+  $("#stop").hide();
+}
+
+function writeQuestion() {
+  for (var i = 0; i < multiQuestion.length; i++) {
+
+    var writeQuestion = $("<br> <p>" + multiQuestion[i] + " </p> <br>");
+    writeQuestion.addClass("styleClass");
+    writeQuestion.attr("data-questionValue", correctAnswers[i]);
+    $(".question").append(writeQuestion);
+
+    for (var j = 0; j < multiAnswers[i].length; j++) {
+      $(".question").append("<input type='radio' class='radio' name='answers" 
+      + [i] + "' value= '" + multiAnswers[i][j] + "'> "
+      + multiAnswers[i][j] + "<br>");
+    }
+  }
+}
+
+//correctAnswer function
+function checkAnswers() {
+  var userInput = $(this).val();
+  console.log(userInput);
+  var name = $(this)[0].name;
+  var key = name.split("answers");
+  console.log(key);
+  var index = key[1];
+  console.log(index);
+  if (userInput === correctAnswers[index]) {
+    correctAnsCounter++; 
+    unAnsweredCounter--;
+    console.log("answers match");
+  } else {
+    incorrAnsCounter++; 
+    unAnsweredCounter--;
+    console.log("answers DON'T match");
+  }
+  console.log("you didn't answer question #" + unAnsweredCounter);
+}
 
 
 
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,111 +1,43 @@
-var sixtySecNum = 10;
-var intervalId;
-var correctAnsCounter = 0;
-var incorrAnsCounter = 0;
+You'll create a trivia form with multiple choice or true/false options 
+(your choice).
 
-var multiQuestion = [
-  "How many slices of pizza did you eat?",
-  "A Question",
-  "AA Question",
-  "AAA Question"
-];
+The player will have a limited amount of time to finish the quiz.
 
-var unAnsweredCounter = multiQuestion.length;
+The game ends when the time runs out. The page will reveal the number 
+of questions that players answer correctly and incorrectly.
 
-var multiAnswers = [
-  ["one",
-  "whole pie",
-  "none",
-  "three"],
-  ["A",
-  "B",
-  "C",
-  "D"],
-  ["AA",
-  "BB",
-  "CC",
-  "DD"],
-  ["AAA",
-  "BBB",
-  "CCC",
-  "DDD"]
-];
-var correctAnswers = [
-  "one",
-  "A",
-  "AA",
-  "AAA",
-];
+Don't let the player pick more than one answer per question.
 
-$("#start").on("click", start);
-$("#stop").on("click", stop);
-$(".question").on("click", "input", checkAnswers);
+Don't forget to include a countdown timer.
 
-function start() {
-  $("#start").hide();
-  run();
-  writeQuestion();
-  $("#stop").append($('<input id="stop" type="button" class="w3-btn w3-round-xlarge" value="STOP">'));
-}
+Pseudo Code: 
 
-function run() {
-  clearInterval(setInterval)
-  intervalId = setInterval(decrement, 1000);
-}
+Start Button - on click 
+    // Timer starts
+    //     counting down timer from 1 min 
+        (after start click create hide function)
 
-function decrement() {
-  sixtySecNum--;
-  $("#timerDiv").html("<h2>" + sixtySecNum + "</h2>");
-  if (sixtySecNum === 0) {
-    stop();
-  }
-}
+Triva Questions are asked
+    List of multiple questions (hardcode in HTML, or with an array in JS & jQeury)
+    List of multiple choice answers (with an array in JS & jQeury)
 
-function stop() {
-  clearInterval(intervalId);
-  $(".question").hide();
-  $("#timerDiv").hide();
-  $(".counters").html("Correct Answers: " + correctAnsCounter + "<br>");
-  $(".counters").append("Wrong Answers: " + incorrAnsCounter + "<br>"); 
-  $(".counters").append("You Didn't Answer: " + unAnsweredCounter + " questions" + "<br>"); 
-}
-
-function writeQuestion() {
-  for (var i = 0; i < multiQuestion.length; i++) {
-
-    var writeQuestion = $("<br> <p>" + multiQuestion[i] + " </p> <br>");
-    writeQuestion.addClass("styleClass");
-    writeQuestion.attr("data-questionValue", correctAnswers[i]);
-    $(".question").append(writeQuestion);
-
-    for (var j = 0; j < multiAnswers[i].length; j++) {
-      $(".question").append("<input type='radio' class='radio' name='answers" 
-      + [i] + "' value= '" + multiAnswers[i][j] + "'> "
-      + multiAnswers[i][j] + "<br>");
-    }
-  }
-}
-
-//correctAnswer function
-function checkAnswers() {
-  var userInput = $(this).val();
-  console.log(userInput);
-  var name = $(this)[0].name;
-  var key = name.split("answers");
-  console.log(key);
-  var index = key[1];
-  console.log(index);
-  if (userInput === correctAnswers[index]) {
-    correctAnsCounter++; 
-    unAnsweredCounter--;
-    console.log("answers match");
-  } else {
-    incorrAnsCounter++; 
-    unAnsweredCounter--;
-    console.log("answers DON'T match");
-  }
-  console.log("you didn't answer question #" + unAnsweredCounter);
-}
+User answers Trivia Questions
+    with a condition of if and else 
+        What to do if the user answers all questiosn in <60 seconds
+            Stop Button 
+                on click listener 
+                after stop click create hide function on click listener
+            Got to results page 
+                Display Answered Questions Counter
+                    Correct Answered
+                    Incorrectly Answered
+                    Unanswered
+        What to do if the user answers all questiosn in >60 seconds
+            Got to results page 
+                Display Answered Questions Counter
+                    Correct Answered 
+                    Incorrectly Answered
+                    Unanswered
 
 
 
